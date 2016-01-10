ecabe2340d98ae6e0e07fe6c467778654dc6fa04
Antoine Bichon <antoinebichon@msn.com>
2016-01-10 18:08:31 +0100

    ajout achievement definition

---
+++ PhaserCordovaGame/scripts/googlegamedev/dataStructure/achievement.ts
@@ -0,0 +1,24 @@
+﻿module GoogleGameDev {
+    export class AchievementDefinitionsListResponse {
+        public kind: string = "games#achievementDefinitionsListResponse";
+        public nextPageToken: string;
+        public items: AchievementDefinitions[];
+    }
+
+    export class AchievementDefinitions {
+        public kind: string = "games#achievementDefinition";
+        public id: string;
+        public name: string;
+        public description: string;
+        public achievementType: string;
+        public totalSteps: number;
+        public formattedTotalSteps: string;
+        public revealedIconUrl: string;
+        public isRevealedIconUrlDefault: boolean;
+        public unlockedIconUrl: string;
+        public isUnlockedIconUrlDefault: boolean;
+        public initialState: string;
+        public experiencePoints: number;
+    }
+
+}
