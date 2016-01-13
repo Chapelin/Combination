42dbc33784ee2c56fdd2b0c36efab1dcce95589a
Antoine Bichon <antoine.bichon@cgi.com>
2016-01-13 13:31:20 +0100

    passage par un APIHelper

---
+++ PhaserCordovaGame/scripts/googlegamedev/apiHelper.ts
@@ -0,0 +1,29 @@
+﻿module GoogleGameDev {
+    export class ApiHelper {
+        token: GoogleApiOAuth2TokenObject;
+        baseUrl: string = "https://www.googleapis.com/games/v1/";
+
+        constructor() {
+            gapi.auth.authorize({
+                client_id: PhaserCordovaGame.Keys.GoogleClientId,
+                scope: 'https://www.googleapis.com/auth/games'
+            }, this.callBackAuth.bind(this)
+            );
+        }
+
+        callBackAuth(token: GoogleApiOAuth2TokenObject) {
+            this.token = token
+          
+        }
+
+        public getListAchievements(callback: (response:AchievementDefinitionsListResponse)  =>  any, context? : any ){
+            gapi.auth.setToken(this.token)
+            gapi.client.request({
+                path: this.baseUrl + "achievements",
+                method: "GET",
+                callback: callback.bind(context)
+            }
+            );
+        }
+    }
+}
--- PhaserCordovaGame/scripts/main.ts
+++ PhaserCordovaGame/scripts/main.ts
@@ -2,20 +2,18 @@
     export class Main extends Phaser.State {
         game: Phaser.Game;
         plateauJoueur: Plateau;
-        token: GoogleApiOAuth2TokenObject;
-        baseUrl: string = "https://www.googleapis.com/games/v1/"
+        apiHelper: GoogleGameDev.ApiHelper
+
         constructor() {
             super();
+            this.apiHelper = new GoogleGameDev.ApiHelper();
 
         }
 
         create() {
-            gapi.auth.authorize({
-                client_id: '634302798162-n2vj530cdd378m4gjs8ukgtkb7jbelak.apps.googleusercontent.com',
-                scope:'https://www.googleapis.com/auth/games'
-            }, this.callBackAuth.bind(this)
-            );
+          
             this.plateauJoueur = new Plateau(this.game, 10, 7);
+            this.apiHelper.getListAchievements(this.logAchievements, this);
 
         }
 
@@ -27,21 +25,11 @@
             this.game.state.start(stateGameOver);
         }
 
-        callBackAuth(token: GoogleApiOAuth2TokenObject) {
-            this.token = token
-            console.log(token);
-            gapi.auth.setToken(token)
-            gapi.client.request({
-                path: this.baseUrl + "achievements",
-                method: "GET",
-                callback: this.test
-            }
-            );
-        }
-
-        test(data?: GoogleGameDev.AchievementDefinitionsListResponse) {
+        logAchievements(data?: GoogleGameDev.AchievementDefinitionsListResponse) {
             console.log(data);
         }
+
+        
     }
 
 }
