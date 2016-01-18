b2671b8d2540b172b2ae050246dea95fa1aa8fc1
Antoine Bichon <antoine.bichon@cgi.com>
2016-01-18 12:37:17 +0100

    gameover : boutons présents

--- PhaserCordovaGame/scripts/engine/states/gameover.ts
+++ PhaserCordovaGame/scripts/engine/states/gameover.ts
@@ -1,21 +1,53 @@
 module PhaserCordovaGame {
     export class GameOver extends Phaser.State {
         game: Phaser.Game;
+        nombreCoup: number;
+        levelNumber: number;
+        textPerdu: Phaser.Text;
+        buttonTry: Phaser.Button;
+        buttonMain: Phaser.Button;
+        defaultStyle: Phaser.PhaserTextStyle;
 
         constructor() {
             super();
+
+            var fontName = "Arial";
+
+            if (window.cordova.platformId === "android") {
+                fontName = "Droid Sans";
+            }
+
+
+            this.defaultStyle = {
+                font: fontName,
+                fill: "#ff0044",
+                fontSize: 35
+
+            }
         }
 
-        init( currentLevel: number) {
 
+        init(currentLevel: number) {
+            this.levelNumber = currentLevel;
+
+            this.textPerdu = new Phaser.Text(this.game, 50, 50, "Dommage.", this.defaultStyle);
+            this.buttonMain = new Phaser.Button(this.game, 50, 300, AssetKeys.assetBoutonRouge, this.startMain, this)
+            this.buttonMain.width = 150;
+            this.buttonTry = new Phaser.Button(this.game, 50, 450, AssetKeys.assetBoutonVert, this.restart, this)
+            this.buttonTry.width = 150;
+            this.game.add.existing(this.textPerdu);
+            this.game.add.existing(this.buttonTry);
+            this.game.add.existing(this.buttonMain);
         }
 
-        create() {
-
+        restart() {
+            this.game.state.start(stateMain, true, false, this.levelNumber);
         }
 
-        restartGame() {
-            this.game.state.start(stateGameTitle);
+        startMain() {
+            this.game.state.start(stateMain);
         }
+
+
     }
 }
