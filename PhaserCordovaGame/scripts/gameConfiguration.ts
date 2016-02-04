7c93fdf06a6a62fccf451f4ecbdc08c36d7903ab
Antoine Bichon <antoinebichon@msn.com>
2016-02-04 20:04:44 +0100

    corectif suite premier essais

--- PhaserCordovaGame/scripts/gameConfiguration.ts
+++ PhaserCordovaGame/scripts/gameConfiguration.ts
@@ -1,6 +1,6 @@
 ﻿module PhaserCordovaGame {
     export class GameConfiguration {
-        public static GAMEANIM_SPEED_FALL = 650;
-        public static GAMEANIM_SPEED_FADE = 300;
+        public static GAMEANIM_SPEED_FALL = 500;
+        public static GAMEANIM_SPEED_FADE = 200;
     }
 }
