44d1fa1b467c3edd9d34adbab3ad54aa3cdaf9a6
Antoine Bichon <antoine.bichon@cgi.com>
2016-07-21 13:20:17 +0200

    choix du mode : poc ok

--- PhaserCordovaGame/scripts/engine/states/chooseMode.ts
+++ PhaserCordovaGame/scripts/engine/states/chooseMode.ts
@@ -2,14 +2,34 @@
     export class ChooseMode extends Phaser.State {
         game: Phaser.Game;
         defaultStyle: any;
+        playInfinite: Phaser.Sprite
+        playPuzzle: Phaser.Sprite
         constructor() {
             super();
-
         }
 
 
         create() {
+            this.playInfinite = new Phaser.Sprite(this.game, 10, 10, AssetKeys.assetLevelBox);
+            this.playPuzzle = new Phaser.Sprite(this.game, 10, 300, AssetKeys.assetLevelBoxUnAvailable);
+
+            this.playInfinite.events.onInputUp.addOnce(this.clickModeInfinite, this);
+            this.playPuzzle.events.onInputUp.addOnce(this.clickModePuzzle, this);
+            this.playInfinite.inputEnabled = true;
+            this.playPuzzle.inputEnabled = true;
+
+            this.game.add.existing(this.playInfinite);
+            this.game.add.existing(this.playPuzzle);
+        }
+
+
+
+        clickModePuzzle() {
             this.game.state.start(stateChooser);
+        }
+
+        clickModeInfinite() {
+            this.game.state.start(statePlayingInfinite);
         }
     }
 }
